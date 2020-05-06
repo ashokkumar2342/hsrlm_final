978feb2a7257118dccae0f3ec3fadb6bd261bb77
ashokkumar2342 <ashokkumar2342@gmail.com>
2020-05-06 13:06:48 +0530

    Update rate_list_farmer_table.blade.php

--- resources/views/admin/master/ratelist/rate_list_farmer_table.blade.php
+++ resources/views/admin/master/ratelist/rate_list_farmer_table.blade.php
@@ -13,13 +13,14 @@
         @php
         	 $RateList=App\Model\RateList::where('for_date',$date)->where('items_id',$Item->id)->first(); 
         	 $purchase_rate =0; 
+           $rate =0; 
           	 if (!empty($RateList)){ 
               $users =Auth::guard('admin')->user();
-              $transaction=App\Model\Transaction::where('user_id',$users->id)->where('for_date',$date)->where('item_id',$Item->id)->first();
-             
+              $transaction=App\Model\Transaction::where('user_id',$users->id)->where('for_date',$date)->where('item_id',$Item->id)->first(); 
               if(!empty($transaction)){
                $Qty=$transaction->qty;
                $transaction_id=$transaction->id;
+               $rate =$transaction->rate;
               }else{
                 $Qty=0;
                 $transaction_id='';
@@ -29,12 +30,12 @@
               }elseif($users->user_type_id==3){
                 $purchase_rate = $RateList->sale_rate;
               } 
-              $p_tolal=$Qty*$purchase_rate;
+              $p_tolal=$Qty*$rate;
         	 }  
         @endphp
         <tr>
            
-            <input type="hidden" name="transaction_id[{{ $Item->id }}]" value="{{ $transaction_id }}">
+            <input type="hidden" name="transaction_id[{{ $Item->id }}]" value="{{ @$transaction_id }}">
             <td>
                 @php
              $itemsImage = route('admin.master.items.image',$Item->picture); 
@@ -46,15 +47,15 @@
            
             <td> 
             	 <b>{{ $purchase_rate }}</b>
-               <input type="hidden" class="form-control sub_total_item" style="width:60px"   value="{{ $purchase_rate }}" required="" name="rate[{{ $Item->id }}]" id="rate">
+               <input type="hidden" class="form-control" style="width:60px"   value="{{ $purchase_rate }}" required="" name="rate[{{ $Item->id }}]" id="rate">
             </td>
              <td>
-            	<input type="number" class="form-control item" style="width:60px"   value="{{ $Qty}}" required="" name="units[{{ $Item->id }}]" id="p_rate_{{ $Item->name }}" oninput="calcPrice(this.value,{{ $purchase_rate }},'p_total_{{ $Item->id }}')">
+            	<input type="number" class="form-control item" style="width:60px"   value="{{ @$Qty}}" required="" name="units[{{ $Item->id }}]" id="p_rate_{{ $Item->name }}" oninput="calcPrice(this.value,{{ $purchase_rate }},'p_total_{{ $Item->id }}')">
                 {{-- <input type="number" class="form-con   trol item" style="width:60px"   value="0" required="" name="purchage_rate[]" id="purchage_rate{{ $Item->id }}"> --}}
             </td>
             <td>
-                <span id="p_total_{{ $Item->id }}">{{ $p_tolal }}</span>
-                <input type="hidden" class="form-control sub_total_item" style="width:60px"   value="{{ $p_tolal }}" required="" name="sub_p_total_[{{ $purchase_rate }}]" id="sub_p_total_{{ $Item->id }}">
+                <span id="p_total_{{ $Item->id }}">{{ @$p_tolal }}</span>
+                <input type="hidden" class="form-control sub_total_item" style="width:60px"   value="{{ @$p_tolal }}" required="" name="sub_p_total_[{{ $purchase_rate }}]" id="sub_p_total_{{ $Item->id }}">
             </td>
 
              
@@ -110,14 +111,14 @@
     });
     $(document).ready(function () { 
         
-           var p_calculated_total_sum = 0; 
+           var p_calculated_total_sum2 = 0; 
            $("#items_table .sub_total_item").each(function () { 
                var get_textbox_value = $(this).val(); 
                if ($.isNumeric(get_textbox_value)) {
-                  p_calculated_total_sum += parseFloat(get_textbox_value);
+                  p_calculated_total_sum2 += parseFloat(get_textbox_value);
                   }                  
                 }); 
-            $("#total_price").html(p_calculated_total_sum);
+            $("#total_price").html(p_calculated_total_sum2);
         });
    
     
